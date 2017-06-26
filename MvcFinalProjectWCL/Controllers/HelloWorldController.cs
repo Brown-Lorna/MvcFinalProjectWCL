14d0120dd027a1fb775a42be99fbc11edd72431e
Brown-Lorna <bro12070@byui.edu>
2017-06-26 12:19:31 -0600

    Hello World

---
+++ MvcFinalProjectWCL/Controllers/HelloWorldController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcFinalProjectWCL.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public string Index()
+        {
+            return "Hello World!";
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome()
+        {
+            return "Welcome to our MVC Project.";
+        }
+    }
+}
+
